06674b7892f865469018a78ab47fbe3646709cec
Anderson Marlon <anderson18.marlon@gmail.com>
2024-07-11 15:26:51 -0300

    Fix: Anchor (Day 01) - 11-07 15:26

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -101,7 +101,7 @@ function App() {
                 </div>
 
                 <p className="text-zinc-500 text-sm">Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
-                    com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a href="#">políticas de privacidade</a>.
+                    com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
                 </p>
 
                 {
