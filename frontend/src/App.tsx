74b7497fba82af2461222a6f0fbbbc553eb07b1c
Anderson Marlon <anderson18.marlon@gmail.com>
2024-07-11 15:26:12 -0300

    Added: addNewEmailFromInvites + removeEmailFromInvites - 11-07 15:26

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from "lucide-react"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 
 function App() {
     const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
     const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
+    const [emailsToInvite, setEmailsToInvite] = useState<string[]>([])
 
     function openGuestsInput() {
         setIsGuestsInputOpen(true)
@@ -19,6 +20,30 @@ function App() {
 
     function closeGuestsModal() {
         setIsGuestsModalOpen(false)
+    }
+
+    function addNewEmailFromInvites(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        const data = new FormData(event.currentTarget)
+        const email = data.get('email')?.toString()
+
+        if (!email) {
+            return
+        }
+
+        if (emailsToInvite.includes(email)) {
+            return
+        }
+
+        setEmailsToInvite([...emailsToInvite, email])
+
+        event.currentTarget.reset()
+    }
+
+    function removeEmailFromInvites(emailToRemove: string) {
+        const newEmailList = emailsToInvite.filter(email => email !== emailToRemove)
+
+        setEmailsToInvite(newEmailList)
     }
 
     return (
@@ -92,19 +117,24 @@ function App() {
                                 </div>
 
                                 <div className="flex flex-wrap gap-2">
-                                    <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
-                                        <span className="text-zinc-300">yagasakiwanderlust@gmail.com</span>
-                                        <button><X className="size-4 text-zinc-400" /></button>
-                                    </div>
+                                    {
+                                        emailsToInvite.map((email) => (
+                                            <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                                                <span className="text-zinc-300">{email}</span>
+                                                <button onClick={() => removeEmailFromInvites(email)}><X className="size-4 text-zinc-400" /></button>
+                                            </div>
+                                        ))
+                                    }
                                 </div>
 
                                 <div className="w-full h-px bg-zinc-800" />
-                                <form className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+
+                                <form onSubmit={addNewEmailFromInvites} className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
                                     <div className="px-2 flex items-center flex-1 gap-2">
                                         <AtSign className="size-5 text-zinc-400" />
-                                        <input type="text" name="" id="" placeholder="Digite o e-mail do  convidado" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1" />
+                                        <input type="email" name="email" id="" placeholder="Digite o e-mail do convidado" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1" />
                                     </div>
-                                    <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                                    <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                                         Convidar
                                         <Plus className="size-5" />
                                     </button>
